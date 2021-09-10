ab6468cd97e3b66e5bf9bb77232720a3e13a311f
pavelzaborin <pavel.zaborin@inbox.ru>
2021-09-10 11:35:37 +0500

    Add folders and config files, change .gitignores file

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+    entry: '.app/app.jsx',
+    output:{
+      path: path.resolve(__dirname, './public/js/'),
+      publicPath: 'https://science-project.ru/js/',
+      filename: '[name].[contenthash].js',
+      clean : true,
+    },
+    devServer: {
+      historyApiFallback: true,
+      port : 3000,
+      open: true,
+    },
+    module: {
+      rules:[
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          options:{
+            presets:['@babel/preset-env', '@babel/preset-react']
+          },
+        },
+        {
+          test: /\.css$/i,
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[path]_[local]--[hash:base62:5]'
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      })
+    ]
+}
